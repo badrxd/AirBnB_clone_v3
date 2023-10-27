1700f6e1920af8570185df9174afde7d47923df5
root <root@DESKTOP-J73E3I4>
2023-10-27 18:59:16 +0100

    task 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """ Api v1 entrypoint index route"""
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
@@ -13,4 +20,11 @@ def status():
 @app_views.route("/stats")
 def count():
     """return stats counts"""
-    return storage.count()
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+
+    counts = {}
+    for key, value in classes.items():
+        counts[key] = storage.count(value)
+    return counts
