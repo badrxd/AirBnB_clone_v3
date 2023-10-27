00a01fd441f58d4496bd5c947603b42156cf09a4
root <root@DESKTOP-J73E3I4>
2023-10-27 19:30:01 +0100

    task 5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Api v1 entrypoint for AirBnB v3 flask project"""
-from flask import Flask
+from flask import Flask, make_response
 from models import storage, storage_t
 from api.v1.views import app_views
 from os import getenv
@@ -8,7 +8,6 @@ from os import getenv
 app = Flask(__name__)
 HBNB_API_HOST = getenv("HBNB_API_HOST")
 HBNB_API_PORT = getenv("HBNB_API_PORT")
-# print(HBNB_API_HOST,HBNB_API_PORT)
 app.register_blueprint(app_views)
 
 
@@ -16,6 +15,16 @@ app.register_blueprint(app_views)
 def teardown_request(error):
     """remove the current SQLAlchemy Session after each request"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """return error 404
+    Responses:
+      404:
+        description: set for not found paths
+    """
+    return make_response({'error': 'Not found'}, 404)
 
 
 if __name__ == '__main__':
