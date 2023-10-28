4daa4e03d0b6c6a9a6e9606023fcfb1fa1d809af
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-10-28 21:25:19 +0100

    upload 4 files

---
+++ api/v1/views/states.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+""" Api v1 entrypoint state route"""
+from flask import abort, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states", methods=["GET"])
+def states():
+    """return list of states"""
+    states = storage.all(State)
+    statesList = []
+
+    for key in states:
+        statesList.append(states[key].to_dict())
+
+    return statesList
+
+
+@app_views.route("/states/<state_id>", methods=["GET"])
+def state_id(state_id):
+    """return one state by id"""
+    state = storage.get(State, state_id)
+
+    if state is None:
+        abort(404)
+    else:
+        return (state.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"])
+def delete_state(state_id):
+    """delete state by id"""
+    state = storage.get(State, state_id)
+
+    if state is None:
+        abort(404)
+
+    state.delete()
+    storage.save()
+    return make_response({}, 200)
+
+
+@app_views.route("/states", methods=["POST"])
+def post_state():
+    """create state by name"""
+    name = request.get_json()
+
+    if not name:
+        abort(400, description="Not a JSON")
+
+    if 'name' not in name:
+        abort(400, description="Missing name")
+
+    state = State(**name)
+    state.save()
+    return make_response(state.to_dict(), 201)
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def put_state(state_id):
+    """update state by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    keys = request.get_json()
+    if not keys:
+        abort(400, description="Not a JSON")
+
+    for key, value in keys.items():
+        if key not in {'id', 'created_at', 'updated_at'}:
+            setattr(state, key, value)
+
+    state.save()
+    return make_response(state.to_dict(), 200)
---
+++ api/v1/views/users.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+""" Api v1 entrypoint user route"""
+from flask import abort, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route("users", methods=["GET"])
+def users():
+    """return users"""
+    usersList = []
+    users = storage.all(User)
+
+    for value in users.values():
+        obj = value.to_dict()
+        usersList.append(obj)
+
+    return usersList
+
+
+@app_views.route("users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    """return user by id"""
+    user = storage.get(User, user_id)
+
+    if user is None:
+        abort(404)
+
+    return user.to_dict()
+
+
+@app_views.route("users/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    """delete user by id"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return make_response({}, 200)
+
+
+@app_views.route("users", methods=["POST"])
+def post_user():
+    """create new user"""
+    data = request.get_json()
+
+    if data is None:
+        abort(400, description="Not a JSON")
+
+    if 'email' not in data:
+        abort(400, description="Missing email")
+
+    if 'password' not in data:
+        abort(400, description="Missing password")
+
+    user = User(**data)
+    user.save()
+    return make_response(user.to_dict(), 201)
+
+
+@app_views.route("users/<user_id>", methods=["PUT"])
+def put_user(user_id):
+    """update user by id"""
+
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+
+    data = request.get_json()
+    if data is None:
+        abort(400, description="Not a JSON")
+
+    IgnoreKeys = {'id', 'email', 'created_at', 'updated_at'}
+    for key, value in data.items():
+        if key not in IgnoreKeys:
+            setattr(user, key, value)
+    storage.save()
+    return make_response(user.to_dict(), 200)
