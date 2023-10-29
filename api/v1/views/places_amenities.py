20fdc735f913271661270df2149a8ee114d30f10
badrxd <badrxdxd@gmail.com>
2023-10-29 21:18:22 +0100

    add delete_amenity_1 methode

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -33,3 +33,30 @@ def get_amenities(place_id):
             amenitiesList.append(obj)
 
     return make_response(json.dumps(amenitiesList), 200)
+
+
+@app_views.route("places/<place_id>/amenities/<amenity_id>", methods=["DELETE"])
+def delete_amenity_1(place_id, amenity_id):
+    """delete amenity by id"""
+    checker = None
+
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+
+    for value in place.amenities:
+        if value.to_dict().get('id') == amenity_id:
+            checker = True
+            break
+
+    if checker is None:
+        abort(404)
+
+    amenity.delete()
+    storage.save()
+
+    return make_response({}, 200)
